0cb69bc809637f38377be05c36acd5d25f64bd54
Valentin <valentin.benitez@beetransfer.net>
2024-12-01 21:32:49 -0300

    update

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCctoUQV99aYWo86KWNKtVMGajoAEvNCzo',
-  authDomain: 'turnos-app-d94db.firebaseapp.com',
-  projectId: 'turnos-app-d94db',
-  storageBucket: 'turnos-app-d94db.firebasestorage.app',
-  messagingSenderId: '678531652269',
-  appId: '1:678531652269:web:89e91b89edae9164b0c424',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
